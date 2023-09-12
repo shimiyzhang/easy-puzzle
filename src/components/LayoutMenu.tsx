aaf9f647ca5f6c06a7bfff00057a752149857562
张书猛 <916406821@qq.com>
2023-09-12 11:50:24 +0800

    feat: 🎸 调整左侧菜单的布局效果

--- src/components/LayoutMenu.tsx
+++ src/components/LayoutMenu.tsx
@@ -2,6 +2,8 @@
 import { useRef, useState } from 'react';
 import ScrollBar, { ScrollBarRef } from './ScrollBar';
 import { layoutList } from '@/data/layoutData';
+import { Button, Dropdown, MenuProps, Select, Space } from 'antd';
+import { DownOutlined, SmileOutlined } from '@ant-design/icons';
 
 export type LayoutMenuProps = {
   activeKey: string;
@@ -23,14 +25,22 @@ export default function LayoutMenu({ activeKey, changeKey }: LayoutMenuProps) {
   const filterList = layoutList.filter(({ children }) => children.length > 0);
   const indexs = filterList.map(({ value }) => value);
 
+  const beforeList = filterList.filter(({ value }) => value <= 6);
+  const afterList = filterList.filter(({ value }) => value > 6);
+
+  const items: MenuProps['items'] = afterList.map(({ value }) => ({
+    key: value,
+    label: value,
+  }));
+
   return (
     <div className='flex h-full items-center'>
       <div className={`h-full overflow-hidden rounded-tr-lg bg-white ${open ? 'w-72' : 'w-0'}`}>
         <div className='relative flex h-full flex-col'>
           <div className='flex flex-wrap pt-4 pb-2 pl-5'>
-            {filterList.map(({ label, value }) => (
+            {beforeList.map(({ label, value }) => (
               <button
-                className={`mr-2 mb-2 rounded-lg border border-gray-100 py-1 px-3 text-xs ${
+                className={`mr-2 mb-2 rounded border border-gray-100 py-1 px-3 text-xs hover:border-[#b2b2b2] ${
                   active === value ? ' bg-gray-400 text-white' : ''
                 }`}
                 key={value}
@@ -39,6 +49,20 @@ export default function LayoutMenu({ activeKey, changeKey }: LayoutMenuProps) {
                 {label}
               </button>
             ))}
+            {afterList.length > 0 && (
+              <Dropdown
+                menu={{ items, onClick: ({ key }) => onChangeActive(Number(key)) }}
+                trigger={['click']}
+              >
+                <button
+                  className='mr-2 mb-2 flex w-12 items-center justify-center rounded border border-gray-100 text-xs
+                hover:border-[#b2b2b2]'
+                >
+                  {active > 6 ? active : '更多'}
+                  <img src='/down-outlined.svg' alt='down-outlined' />
+                </button>
+              </Dropdown>
+            )}
           </div>
           {/* scroll-bar */}
           <ScrollBar
